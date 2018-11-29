d311440760d39e1e1caab995e781a1914bdb9a0f
pavelteplitsky <pavel.teplitsky@groupdocs.com>
2018-11-29 13:25:44 +0200

    Added xsp

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,9 +1,7 @@
-﻿using Owin;
-
-namespace GroupDocs.Total.WebForms
+﻿namespace GroupDocs.Total.WebForms
 {
     public partial class Startup {
-        public void Configuration(IAppBuilder app) {
+        public void Configuration() {
            
         }
     }
