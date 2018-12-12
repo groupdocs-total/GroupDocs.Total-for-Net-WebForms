dc75796945f2ef31cde6a2fe3beba74ecf4c26ff
pavelteplitsky <pavel.teplitsky@groupdocs.com>
2018-12-12 11:33:24 +0200

    Added initial version of the test project

---
+++ GroupDocs.Total.WebForms.Test/ViewerTest.cs
@@ -0,0 +1,33 @@
+﻿using Huygens;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace GroupDocs.Total.WebForms.Test
+{
+    [TestFixture]
+    public class ViewerTest
+    {
+        [Test]
+        public void ViewStatusTest()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";
+            using (var server = new DirectServer(path))
+            {
+                var request = new SerialisableRequest
+                {
+                    Method = "POST",
+                    RequestUri = "/viewer/loadFileTree",
+                    Headers = new Dictionary<string, string>{
+                        { "Content-Type","application/json" }
+                    },
+
+                    Content = null
+                };
+
+                var result = server.DirectCall(request);
+                Assert.That(result.StatusCode, Is.EqualTo(200));
+            }
+        }
+    }
+}
