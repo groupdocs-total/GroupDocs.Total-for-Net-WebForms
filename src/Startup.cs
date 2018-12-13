f99a4b536b6e1b07e20ef7d3e964b84fcc914bd0
pavelteplitsky <pavel.teplitsky@groupdocs.com>
2018-12-13 08:55:37 +0200

    Added view status code tests

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,9 +1,8 @@
-﻿using Owin;
-
+﻿
 namespace GroupDocs.Total.WebForms
 {
     public partial class Startup {
-        public void Configuration(IAppBuilder app) {
+        public void Configuration() {
            
         }
     }
