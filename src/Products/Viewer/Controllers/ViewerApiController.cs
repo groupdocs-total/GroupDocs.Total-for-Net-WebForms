1f18271171e1534cd93179fac25d37bf5544d7b9
pavelteplitsky <pavel.teplitsky@aspose.com>
2019-05-30 11:00:39 +0300

    Added angular support viewer

--- src/Products/Viewer/Controllers/ViewerApiController.cs
+++ src/Products/Viewer/Controllers/ViewerApiController.cs
@@ -1,5 +1,6 @@
 ﻿using GroupDocs.Total.WebForms.Products.Common.Entity.Web;
 using GroupDocs.Total.WebForms.Products.Common.Resources;
+using GroupDocs.Total.WebForms.Products.Viewer.Config;
 using GroupDocs.Total.WebForms.Products.Viewer.Entity.Web;
 using GroupDocs.Viewer.Config;
 using GroupDocs.Viewer.Converter.Options;
@@ -58,6 +59,17 @@ namespace GroupDocs.Total.WebForms.Products.Viewer.Controllers
                 // initialize Viewer instance for the Image mode
                 viewerImageHandler = new ViewerImageHandler(config);
             }
+        }
+
+        /// <summary>
+        /// Load Viewr configuration
+        /// </summary>       
+        /// <returns>Viewer configuration</returns>
+        [HttpGet]
+        [Route("viewer/loadConfig")]
+        public ViewerConfiguration LoadConfig()
+        {
+            return globalConfiguration.Viewer;
         }
 
         /// <summary>
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp')
- gulp.task('build', function() {
-  return gulp
-    .src('./node_modules/@groupdocs.examples.jquery/**')
-    .pipe(gulp.dest('./Resources/'))
-}) 
+gulp.task('build', function () {
+    return gulp
+        .src('./node_modules/@groupdocs.examples.jquery/**')
+        .pipe(gulp.dest('./Resources/'))
+})
+
+gulp.task('copy', function () {
+    return gulp
+        .src('./node_modules/@groupdocs.examples.angular/viewer/dist/**')
+        .pipe(gulp.dest('./Resources/angular'))
+})
